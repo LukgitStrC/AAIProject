3666159e4a8ef4168f9368066990f71c6bbc75be
KaukasusKiwi <jani.nicke@gmail.com>
2026-01-28 19:35:33 +0100

    Neu trainierte YOLOs mit backgroudn bildern (train2: 91 Epochs von 300, train3: 100 epochs, train 4:198 Epochs von 300)

--- Part1_satelliteImages/dataset_caen/yolo/dataset/train_yolo.py
+++ Part1_satelliteImages/dataset_caen/yolo/dataset/train_yolo.py
@@ -1,6 +1,4 @@
 from ultralytics import YOLO
-from ultralytics.data.split import autosplit
-
 
 
 if __name__ == '__main__':
@@ -8,6 +6,4 @@ if __name__ == '__main__':
 
     model = YOLO("yolo11n-seg.pt")
 
-    img_fp = "images"
-
-    results = model.train(data="data.yaml", epochs=100, imgsz=512, batch=16, device=0)
+    results = model.train(data="data.yaml", epochs=300, imgsz=512, batch=16, device=0)
